d2242be5e1dc04d4ba1733806df79a009dc71804
Vinamra <agarwal.vinamra0405@gmail.com>
2025-05-31 15:50:32 +0530

    Settings Page Setup

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -16,7 +16,7 @@ export const api = createApi({
     }
   }),
   reducerPath: "api",
-  tagTypes: [],
+  tagTypes: ["Managers", "Tenants"],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
       queryFn: async(_, _queryApi, _extraoptions, fetchWithBQ) => {
@@ -42,7 +42,7 @@ export const api = createApi({
               idToken,
               userRole,
               fetchWithBQ
-            )
+            );
           }
 
           return {
@@ -55,12 +55,31 @@ export const api = createApi({
         } catch (error: any) {
           return {error: error.message || "Could not fetch user data"}
         }
-      }
-    })
+      },
+    }),
+
+    updateTenantSettings: build.mutation<Tenant, {cognitoId: string} & Partial<Tenant>>({
+      query: ({cognitoId, ...updatedTenant}) => ({
+        url: `tenants/${cognitoId}`,
+        method: "PUT",
+        body: updatedTenant,
+      }),
+      invalidatesTags: (result) => [{  type: "Tenants", id: result?.id }],
+    }),
+
+    updateManagerSettings: build.mutation<Manager, {cognitoId: string} & Partial<Manager>>({
+      query: ({cognitoId, ...updatedManager}) => ({
+        url: `managers/${cognitoId}`,
+        method: "PUT",
+        body: updatedManager,
+      }),
+      invalidatesTags: (result) => [{  type: "Managers", id: result?.id }],
+    }),
   }),
 });
 
 export const {
   useGetAuthUserQuery,
-
+  useUpdateTenantSettingsMutation,
+  useUpdateManagerSettingsMutation,
 } = api;
